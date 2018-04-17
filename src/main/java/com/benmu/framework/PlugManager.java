2bcb3760b46eca418af24d77b4bcfb7a4c02ba9b
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-04-17 15:34:33 +0800

    commit  register amap module compnent

--- src/main/java/com/benmu/framework/PlugManager.java
+++ src/main/java/com/benmu/framework/PlugManager.java
@@ -1,7 +1,11 @@
 package com.benmu.framework;
 
+import com.benmu.framework.constant.Constant;
 import com.benmu.framework.manager.Manager;
 import com.benmu.framework.manager.ManagerFactory;
+import com.benmu.framework.manager.impl.CustomerEnvOptionManager;
+
+import java.util.HashMap;
 
 /**
  * Created by liuyuanxiao on 17/12/5.
@@ -12,6 +16,24 @@ public class PlugManager {
         Manager geoManager = ManagerFactory.getManagerService("com.plugamap.manager.GeoManager");
         if (geoManager != null) {
             geoManager.init();
+            registerCompnentsAndModules(geoManager.getComponentsAndModules());
+        }
+    }
+
+    /**
+     * 注册 compent 和 Modules
+     *
+     * @param compentAndModules compent 和 Modules 集合
+     */
+    private static void registerCompnentsAndModules(HashMap<String, HashMap<String, String>> compentAndModules) {
+        if (compentAndModules == null) return;
+        HashMap<String, String> compents = compentAndModules.get(Constant.CUSTOMER_COMPONETS);
+        if (compents != null) {
+            CustomerEnvOptionManager.registerComponents(compents);
+        }
+        HashMap<String, String> modules = compentAndModules.get(Constant.CUSTOMER_MODULES);
+        if (modules != null) {
+            CustomerEnvOptionManager.registerModules(compents);
         }
     }
 }
